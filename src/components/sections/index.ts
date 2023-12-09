24e508e12c70189455d31eaf6f2acb44288c3c0c
bernardo <bernardomrl@icloud.com>
2023-12-09 20:12:14 -0300

    added import aliases

---
+++ src/components/sections/index.ts
@@ -0,0 +1,4 @@
+export { default as About } from '@/components/sections/About';
+export { default as Contact } from '@/components/sections/Contact';
+export { default as Hero } from '@/components/sections/Hero';
+export { default as Projects } from '@/components/sections/Projects';
---
+++ src/components/utils/index.ts
@@ -0,0 +1,3 @@
+export { default as ScrollUpButton } from '@/components/utils/ScrollUpButton';
+export { default as CodeMockup } from '@/components/utils/CodeMockup';
+export { default as Section } from '@/components/utils/Section';
